c289aacc99d4889e0e92260fcd0e3473ad3d7549
Jay Kania <120383316+jpkOSTEP@users.noreply.github.com>
2025-04-18 22:50:38 -0400

    Pushing all the files that I could find from the old machine

---
+++ README.md
@@ -0,0 +1,2 @@
+# lone-flyer
+This repo contains some code and media files that I could recover from my old machine.
---
+++ code/mail.py
@@ -0,0 +1,38 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEImage import MIMEImage
+
+#only gmail account will work
+fromEmail = 'sendermail@gmail.com (sender mail)'
+
+
+fromEmailPassword = 'email_password'
+
+
+toEmail = 'receivermail@gmail.com (receiver email)'
+
+def sendEmail(image):
+	msgRoot = MIMEMultipart('related')
+	msgRoot['Subject'] = 'Security Update'
+	msgRoot['From'] = fromEmail
+	msgRoot['To'] = toEmail
+	msgRoot.preamble = 'Raspberry pi security camera update'
+
+	msgAlternative = MIMEMultipart('alternative')
+	msgRoot.attach(msgAlternative)
+	msgText = MIMEText('Smart security cam found object')
+	msgAlternative.attach(msgText)
+
+	msgText = MIMEText('<img src="cid:image1">', 'html')
+	msgAlternative.attach(msgText)
+
+	msgImage = MIMEImage(image)
+	msgImage.add_header('Content-ID', '<image1>')
+	msgRoot.attach(msgImage)
+
+	smtp = smtplib.SMTP('smtp.gmail.com', 587)
+	smtp.starttls()
+	smtp.login(fromEmail, fromEmailPassword)
+	smtp.sendmail(fromEmail, toEmail, msgRoot.as_string())
+	smtp.quit()
